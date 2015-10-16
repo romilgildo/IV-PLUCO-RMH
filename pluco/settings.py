99e37b08a2d9557f5257d8971cb90fe10dc46b71
romilgildo <rubenmartin1991@gmail.com>
2015-10-16 22:50:14 +0200

    Añado mysql a settings.py

--- pluco/settings.py
+++ pluco/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pluco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'plucodb'
+        'USER': 'root',
+        'PASSWORD': 'rumahi885115',
+        'HOST': 'pluco-db.cloudapp.net',
+        'PORT': '5432',
     }
 }
 
