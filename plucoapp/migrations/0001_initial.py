191bc62641c67e5330cebba49f13d56fafb9c7c2
romilgildo <rubenmartin1991@gmail.com>
2015-10-18 21:50:31 +0200

    Meto modelos a la app

---
+++ plucoapp/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Asignatura',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Estudiante',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Profesor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=200)),
+            ],
+        ),
+    ]
--- plucoapp/models.py
+++ plucoapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+	nombre = models.CharField (max_length=200)
+	
+	def __unicode__(self):
+		return self.nombre
+
+class Profesor(models.Model):
+	nombre = models.CharField (max_length=200)
+	
+	def __unicode__(self):
+		return self.nombre
+	
+class Asignatura(models.Model):
+	nombre = models.CharField (max_length=200)
+	
+	def __unicode__(self):
+		return self.nombre
+	
+	
