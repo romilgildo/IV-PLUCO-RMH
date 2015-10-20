a4ad754afa05ac98ccca0e3c9600b46438ebf606
romilgildo <rubenmartin1991@gmail.com>
2015-10-20 11:00:39 +0200

    pruebo test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,9 @@
-# Test para pasar
+def setup_func():
+    "set up test fixtures"
 
+def teardown_func():
+    "tear down test fixtures"
+
+@with_setup(setup_func, teardown_func)
 def test():
-    pass
+    "test ..."
