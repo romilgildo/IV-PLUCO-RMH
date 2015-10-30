9f54e1467f8576a3d6205121fd8654141b2a4db8
romilgildo <rubenmartin1991@gmail.com>
2015-10-30 16:24:45 +0100

    Fusionando carpetas pluco

--- plucoapp/settings.py
+++ plucoapp/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'pluco.urls'
+ROOT_URLCONF = 'plucoapp.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pluco.wsgi.application'
+WSGI_APPLICATION = 'plucoapp.wsgi.application'
 
 
 # Database
--- plucoapp/wsgi.py
+++ plucoapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pluco.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plucoapp.settings")
 
 application = get_wsgi_application()
