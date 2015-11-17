b1a03af4fd10c40d2e0441065a24ebd8293430b3
romilgildo <rubenmartin1991@gmail.com>
2015-11-17 20:39:18 +0100

    vuelvo atras

--- plucoapp/settings.py
+++ plucoapp/settings.py
@@ -105,12 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
