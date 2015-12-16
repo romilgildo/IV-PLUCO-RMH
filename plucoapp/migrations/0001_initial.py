033751a39d91954b48fd470fb6dccc9d01c9f759
romilgildo <rubenmartin1991@gmail.com>
2015-12-16 21:25:48 +0100

    arreglo migraciones django

--- plucoapp/migrations/0001_initial.py
+++ plucoapp/migrations/0001_initial.py
@@ -2,33 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Asignatura',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('nombre', models.CharField(max_length=200)),
+                ('nombre', models.CharField(max_length=100)),
+                ('nombre_id', models.CharField(default=b'nombre_id', max_length=30, serialize=False, primary_key=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Estudiante',
+            name='Usuario',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('nombre', models.CharField(max_length=200)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Profesor',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('nombre', models.CharField(max_length=200)),
+                ('nombre', models.CharField(max_length=100)),
+                ('email', models.EmailField(max_length=254, serialize=False, primary_key=True)),
+                ('tipo', models.CharField(default=b'ESTUDIANTE', max_length=10, choices=[(b'ESTUDIANTE', b'Estudiante'), (b'PROFESOR', b'Profesor')])),
+                ('nick', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
             ],
         ),
     ]
